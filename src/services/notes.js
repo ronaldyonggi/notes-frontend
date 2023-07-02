a78c1d4e45a06856aec7ab3caab2dc1e69ec9455
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-07-02 08:30:02 -0400

    update module exports

--- src/services/notes.js
+++ src/services/notes.js
@@ -42,6 +42,7 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { 
-  getAll, create, update 
+  getAll, create, update , setToken
 }
