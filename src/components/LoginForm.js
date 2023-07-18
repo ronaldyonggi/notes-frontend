aaf659abd543abf2d8714c802ab3502a93c3d579
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-07-18 12:28:11 -0400

    username and password moved to LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,30 @@
-const LoginForm = ({
-  handleSubmit,
-  handleUsernameChange,
-  handlePasswordChange,
-  username,
-  password
-}) => {
+import { useState } from "react"
+
+const LoginForm = ({ login }) => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleLogin = async event => {
+    event.preventDefault()
+
+    // Calls the handleLogin function from app.js
+    await login(username, password)
+
+    setUsername('')
+    setPassword('')
+  }
   return (
     <div>
       <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleLogin}>
         <div>
           username
-          <input value={username} onChange={handleUsernameChange}/>
+          <input value={username} onChange={e => setUsername(e.target.value)}/>
         </div>
 
         <div>
           password
-          <input type="password" value={password} onChange={handlePasswordChange}/>
+          <input type="password" value={password} onChange={e => setPassword(e.target.value)}/>
         </div>
 
         <button type="submit">login</button>
