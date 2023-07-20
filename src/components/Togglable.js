63c0e308dfdaffce92abeff8de71eb98cfc8b8d5
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-07-20 15:44:24 -0400

    make toggleVisibility available outside component

--- src/components/Togglable.js
+++ src/components/Togglable.js
@@ -1,12 +1,18 @@
-const { useState } = require("react")
+const { useState, forwardRef, useImperativeHandle } = require("react")
 
-const Togglable = (props) => {
+const Togglable = forwardRef((props, refs) => {
   const [visible, setVisible] = useState(false)
 
   // Toggle visibility
   const toggleVisibility = () => {
     setVisible(!visible)
   }
+
+  useImperativeHandle(refs, () => {
+    return {
+      toggleVisibility
+    }
+  })
 
   return (
     <div>
@@ -21,6 +27,6 @@ const Togglable = (props) => {
       }
     </div>
   )
-}
+})
 
 export default Togglable
