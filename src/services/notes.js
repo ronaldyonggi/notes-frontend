93fe54b3e126ff1408393cd5b58a498e7757b467
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-07-31 15:39:41 -0400

    removed no-anonymous-default-export

--- src/services/notes.js
+++ src/services/notes.js
@@ -42,7 +42,6 @@ const update = (id, newObject) => {
   return request.then(response => response.data)
 }
 
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { 
+export default {
   getAll, create, update , setToken
 }
