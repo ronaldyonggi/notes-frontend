5f01e2c0a1bdef3a8bba03f74d2eea8e03b935e9
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-09-03 11:12:59 +0700

    add test for clicking button using mock function

--- src/components/Note.test.js
+++ src/components/Note.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '@testing-library/jest-dom'
 import Note from './Note'
+import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/react'
 
 test('renders content', () => {
@@ -13,4 +14,23 @@ test('renders content', () => {
 
   const element = screen.getByText('Component testing is done with react-testing-library')
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = jest.fn()
+
+  render(
+    <Note note={note} toggleImportance={mockHandler}/>
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
