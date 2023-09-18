29e6a3b9733004243937d02b4f72d12d8aee9ec6
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-09-18 14:41:20 +0700

    user login separated to a separate it statement

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -3,14 +3,16 @@ describe('Note app', function() {
     cy.visit('http://localhost:3000')
   })
 
-
   it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
   })
 
-
   it('login form can be opened', function() {
+    cy.contains('log in').click()
+  })
+
+  it('user can login', function() {
     cy.contains('log in').click()
     // cy.get('input:first').type('mluukkai')
     // cy.get('input:last').type('salainen')
