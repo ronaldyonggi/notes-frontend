d292ce42e222711718d09a2e0b9a60008c89c3ca
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-09-18 14:41:33 +0700

    add test for creating note

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -21,5 +21,23 @@ describe('Note app', function() {
     cy.get('#login-button').click()
 
     cy.contains('Matti Luukkainen logged in')
+
+  })
+
+  describe('when logged in', function() {
+    beforeEach(function() {
+      cy.contains('log in').click()
+      cy.get('input:first').type('mluukkai')
+      cy.get('input:last').type('salainen')
+      cy.get('#login-button').click()
+    })
+
+    it('a new note can be created', function() {
+      cy.contains('new note').click()
+      cy.get('input').type('a note created by cypress')
+      cy.contains('save').click()
+      cy.contains('a note created by cypress')
+    })
+
   })
 })
