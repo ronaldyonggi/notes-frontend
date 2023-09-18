300c738e6e41539c56a909b4a1ff741cb4b640da
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-09-18 14:26:33 +0700

    test for log in user

--- cypress/e2e/note_app.cy.js
+++ cypress/e2e/note_app.cy.js
@@ -1,8 +1,23 @@
 describe('Note app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000')
+  })
+
+
+  it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('Note app, Department of Computer Science, University of Helsinki 2022')
   })
 
+
+  it('login form can be opened', function() {
+    cy.contains('log in').click()
+    // cy.get('input:first').type('mluukkai')
+    // cy.get('input:last').type('salainen')
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
+    cy.get('#login-button').click()
+
+    cy.contains('Matti Luukkainen logged in')
+  })
 })
