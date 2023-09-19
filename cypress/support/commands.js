919fa0dd06b5861fc1348c0f2694625748469bcf
Ronald Yonggi <ronaldyonggi@gmail.com>
2023-09-19 18:14:37 +0700

    add custom command for creating new note

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -20,6 +20,19 @@ Cypress.Commands.add('login', ({ username, password }) => {
     })
 })
 
+Cypress.Commands.add('createNote', ({ content, important }) => {
+  cy.request({
+    url: 'http://localhost:3001/api/notes',
+    method: 'POST',
+    body: { content, important },
+    headers: {
+      'Authorization': `Bearer ${JSON.parse(localStorage.getItem('loggedNoteappUser')).token}`
+    }
+  })
+
+  cy.visit('http://localhost:3000')
+})
+
 //
 // -- This is a child command --
 // Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
